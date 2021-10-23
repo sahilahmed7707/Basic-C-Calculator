fa605f1b298a1ae479871b6de75746d55939484d
God Speed <93019921+godspeed5611@users.noreply.github.com>
2021-10-23 08:55:45 +0530

    Created Varidic Template.cpp

---
+++ varadic-template.cpp
@@ -0,0 +1,14 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+template <typename T> void print(T&& arg1) {cout << arg1 << endl;}
+template <typename T, typename... Args>
+void print(T&& arg1, Args&&... args) {cout << arg1 << " "; print(args...); }
+
+int main() {
+#ifndef ONLINE_JUDGE
+	freopen("in.txt", "r", stdin);
+	//freopen("out.txt","w",stdout);
+#endif
+	return 0 ;
+}
